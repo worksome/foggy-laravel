a8b7b3f1aeddafc826717c760a71f151a6b5d0bf
Owen Voke <development@voke.dev>
2024-09-20 12:28:18 +0100

    tests: add new command test

---
+++ tests/Feature/DatabaseDumpCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Worksome\FoggyLaravel\DatabaseDumpCommand;
+
+beforeEach(function () {
+    file_put_contents(base_path('foggy.json'), <<<'JSON'
+    {
+        "database": {
+            "*": {
+                "withData": true
+            }
+        }
+    }
+    JSON
+    );
+});
+
+afterEach(function () {
+    if (file_exists($foggyConfig = base_path('foggy.json'))) {
+        unlink($foggyConfig);
+    }
+});
+
+it('can run the `db:dump` command', function () {
+    $this
+        ->artisan(DatabaseDumpCommand::class)
+        ->expectsOutputToContain('SET NAMES utf8mb4 ;')
+        ->assertOk();
+});
